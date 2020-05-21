f3ff2c836f37cac23900fedf701b20e66d7b4ed8
ps68060 <pslegg@scubadivers.co.uk>
2020-05-21 22:43:47 +0100

    Move getEvent to unit Event.

--- units/event.pas
+++ units/event.pas
@@ -9,7 +9,8 @@ unit Event;
 
 interface
   uses
-    Objects;
+    Objects,
+    Logger;
 
 
 type
@@ -20,17 +21,23 @@ type
     description : String;
     dtStart     : String;
     dtEnd       : String;
+    alarm       : Boolean;
 
     constructor init;
     destructor  done; virtual;
 
+    Function GetEvent (VAR calFile : Text)
+            : Boolean;
+
   end;
+
 
 implementation
 
   constructor TEvent.init;
   begin
     created     := '';
+    summary     := '';
     description := '';
     dtstart     := '';
     dtend       := '';
@@ -41,5 +48,90 @@ implementation
 
   end;
 
+
+  Function TEvent.GetEvent (VAR calFile : Text)
+          : Boolean;
+
+  (*
+    Purpose : Get one iCS event.
+   *)
+
+  var
+    logger       : PLogger;
+
+    checkEnd,
+    convStr      : String;
+
+    currentLn    : String;
+    currentCount : Integer;
+
+    endEvent     : Boolean;
+
+  begin
+    new(logger);
+    logger^.init;
+
+    logger^.level := INFO;
+
+    checkEnd     := 'END:VEVENT';
+    endEvent     := FALSE;
+
+    while (NOT eof (calFile) 
+           AND NOT endEvent )
+    do
+    begin
+
+      readln ( calFile, currentLn );
+      logger^.log (DEBUG, currentLn);
+
+      (* Look for End Event *)
+      if ( pos(checkEnd, currentLn) = 1 )
+      then
+      begin
+
+        endEvent := TRUE;
+
+      end
+      else
+      begin
+        if ( pos('CREATED:', currentLn) = 1 )
+        then
+          created := COPY (currentLn, 9, length(currentLn));
+
+        if ( pos('DTSTART:', currentLn) = 1 )
+        then
+          dtStart := COPY (currentLn, 9, length(currentLn));
+
+        if ( pos('DTEND:', currentLn) = 1 )
+        then
+          dtEnd   := COPY (currentLn, 7, length(currentLn));
+
+        if ( pos('SUMMARY:', currentLn) = 1 )
+           and (NOT alarm)
+        then
+          summary := COPY (currentLn, 9, length(currentLn));
+
+        if ( pos('DESCRIPTION:', currentLn) = 1 )
+           and (NOT alarm)
+        then
+          description := COPY (currentLn, 13, length(currentLn));
+
+        if (pos('BEGIN:VALARM', currentLn) = 1 )
+        then
+          alarm := TRUE;
+
+        if (pos('END:VALARM', currentLn) = 1 )
+        then
+          alarm := FALSE;
+
+      end;  (* if *)
+
+    end;  (* while *)
+
+    Dispose(logger, Done);
+
+    GetEvent := TRUE;
+
+  end;
 
 end.
